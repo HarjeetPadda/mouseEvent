414b34c237a179a41200478d15662e7652358a3b
HarjeetPadda <harjeetpadda@gmail.com>
2023-01-21 21:20:22 +1100

    init

---
+++ index.js
@@ -0,0 +1,24 @@
+function alertMessage() {
+  addEventListener("click", function (e) {
+    console.log(e.clientX, e.clientY);
+    const mainDiv = document.getElementById("mainDiv");
+    const magicDiv = document.createElement("div");
+    magicDiv.style.position = "absolute";
+    magicDiv.style.top = e.clientY + "px";
+    magicDiv.style.left = e.clientX + "px";
+    magicDiv.style.width = "50px";
+    magicDiv.style.height = "50px";
+    magicDiv.style.backgroundColor = "blue";
+    magicDiv.style.borderRadius = "50%";
+    magicDiv.style.zIndex = "999";
+    magicDiv.style.transition = "all 0.5s ease";
+    magicDiv.style.transform = "scale(0)";
+    mainDiv.appendChild(magicDiv);
+    setTimeout(() => {
+      magicDiv.style.transform = "scale(1)";
+    }, 100);
+    setTimeout(() => {
+      mainDiv.removeChild(magicDiv);
+    }, 1000);
+  });
+}
